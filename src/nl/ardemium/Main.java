25b88ada4ba0d51874420373e4467dfdab76d13e
Ardemium <ArthurDjamardzhashvili@outlook.com>
2022-03-29 21:50:40 +0200

    SiezoApp UML

--- src/nl/ardemium/Main.java
+++ src/nl/ardemium/Main.java
@@ -3,13 +3,6 @@ package nl.ardemium;
 public class Main {
 
     public static void main(String[] args) {
-        Participant p = new Participant("Arthur");
 
-        ShareOrder s = new ShareOrder(Menu.getMenuInstance().getDishList().get(1), 3);
-        p.mkOrder(new Order(s));
-        s = new ShareOrder(Menu.getMenuInstance().getDishList().get(2), 2);
-        p.mkOrder(new Order(s));
-
-        System.out.println(p.toString());
     }
 }
